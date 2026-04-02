34bf5e59575d123453a08d739a984aa2023dc9ac
Chris Wong <chris.wong.ksl@gmail.com>
2026-04-02 13:03:54 +0800

    Empty DataJpaTest

--- src/test/java/com/heroku/java/repositories/QuoteRepositoryTests.java
+++ src/test/java/com/heroku/java/repositories/QuoteRepositoryTests.java
@@ -1,18 +1,13 @@
 package com.heroku.java.repositories;
 
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
-// import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@RunWith(SpringRunner.class)
-// @DataJpaTest
+@DataJpaTest
 public class QuoteRepositoryTests {
-  // @Autowired
-  // QuoteRepository quoteRepository;
+  @Autowired
+  QuoteRepository quoteRepository;
 
   @Test
   public void test() throws Exception {
