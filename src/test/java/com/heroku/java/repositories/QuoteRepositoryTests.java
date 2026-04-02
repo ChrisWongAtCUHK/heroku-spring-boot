e3702821d94ec6170cdfc70d993dbf0876ff7039
Chris Wong <chris.wong.ksl@gmail.com>
2026-04-02 13:01:19 +0800

    Empty test

---
+++ src/test/java/com/heroku/java/repositories/QuoteRepositoryTests.java
@@ -0,0 +1,21 @@
+package com.heroku.java.repositories;
+
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+
+@RunWith(SpringRunner.class)
+// @DataJpaTest
+public class QuoteRepositoryTests {
+  // @Autowired
+  // QuoteRepository quoteRepository;
+
+  @Test
+  public void test() throws Exception {
+    // long count = quoteRepository.count();
+  }
+}
