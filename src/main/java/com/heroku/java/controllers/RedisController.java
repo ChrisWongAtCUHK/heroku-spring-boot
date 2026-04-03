a3594467bb32fd8d2317357056b70856b680f939
Chris Wong <chris.wong.ksl@gmail.com>
2026-04-03 10:53:19 +0800

    Connect to Redis Docker server

---
+++ src/main/java/com/heroku/java/controllers/RedisController.java
@@ -0,0 +1,44 @@
+package com.heroku.java.controllers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.Cursor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RedisController {
+
+  @Autowired
+  private RedisTemplate<String, Object> redisTemplate;
+
+  @GetMapping("/redis/keys")
+  public Set<String> getAllKeys() {
+    // "*" is the wildcard pattern to match all keys
+    return redisTemplate.keys("*");
+  }
+
+  @GetMapping("/redis/scanAllKeys")
+  public Set<String> scanAllKeys() {
+    Set<String> keys = new HashSet<>();
+    ScanOptions scanOptions = ScanOptions.scanOptions().match("*").count(100).build();
+
+    // RedisConnection must be closed manually if obtained from the factory
+    try (RedisConnection connection = redisTemplate.getConnectionFactory().getConnection()) {
+      // The method scan(ScanOptions) from the type DefaultedRedisConnection is deprecated
+      // Cursor<byte[]> cursor = connection.scan(scanOptions);
+      Cursor<byte[]> cursor = connection.keyCommands().scan(scanOptions);
+      while (cursor.hasNext()) {
+        keys.add(new String(cursor.next()));
+      }
+    } catch (Exception e) {
+      throw new RuntimeException("Error scanning Redis keys", e);
+    }
+    return keys;
+  }
+}
