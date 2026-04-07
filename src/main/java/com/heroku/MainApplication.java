ae2ea3ba48f852ae09536a938ad08cb77170474d
Chris Wong <chris.wong.ksl@gmail.com>
2026-04-07 10:30:52 +0800

    View a pdf

--- src/main/java/com/heroku/MainApplication.java
+++ src/main/java/com/heroku/MainApplication.java
@@ -2,6 +2,11 @@ package com.heroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -20,14 +25,23 @@ public class MainApplication {
     public String hello(Map<String, Object> model, String input) {
         // http://localhost:5000/hello?input=testspringboot
         try {
-            if(input == null) {
+            if (input == null) {
                 input = "N/A";
             }
             model.put("message", input);
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         return "index";
+    }
+
+    @GetMapping("/view-pdf")
+    public ResponseEntity<Resource> viewPdf() {
+        Resource pdf = new ClassPathResource("static/sample.pdf");
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_PDF)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"sample.pdf\"")
+                .body(pdf);
     }
 
     public static void main(String[] args) {
